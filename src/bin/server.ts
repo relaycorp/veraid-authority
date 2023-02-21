9d0c0684059be0b05e19750a9d6b761447d6f918
annaavannesyan <annaavanesyan@users.noreply.github.com>
2023-02-21 17:07:34 +0400

    returned running server with await

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,6 +1,4 @@
 import { runFastify } from '../services/fastify.js';
 import { makeServer } from '../services/server.js';
 
-(async function startServer(): Promise<void> {
-  await runFastify(await makeServer());
-})();
+await runFastify(await makeServer());
