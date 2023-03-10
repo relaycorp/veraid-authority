1e63949be019435ad7302d7c8951e2b12ff85ab4
levan basharuli <l.basharuli@gmail.com>
2023-03-10 13:31:18 +0400

    added ascii tests

--- src/org.spec.ts
+++ src/org.spec.ts
@@ -40,6 +40,56 @@ describe('org', () => {
     expect(dbResult).not.toBeNull();
   });
 
+  test('Valid data with non ASCII name be stored', async () => {
+    const connection = getConnection();
+    const nonAsciiName = 'はじめよう.みんな';
+    const orgData: OrgSchema = {
+      name: nonAsciiName,
+      memberAccessType: 'INVITE_ONLY',
+      awalaEndpoint: AWALA_ENDPOINT,
+    };
+
+    await createOrg(orgData, {
+      dbConnection: connection,
+      logger: mockLogging.logger,
+    });
+
+    const orgModel = getModelForClass(OrgModelSchema, {
+      existingConnection: connection,
+    });
+    const dbResult = await orgModel.exists({
+      name: nonAsciiName,
+      memberAccessType: MemberAccessType.INVITE_ONLY,
+      awalaEndpoint: AWALA_ENDPOINT,
+    });
+    expect(dbResult).not.toBeNull();
+  });
+
+  test('Valid data with non ASCII Awala endpoint be stored', async () => {
+    const connection = getConnection();
+    const nonAsciiAwalaEndpoint = 'はじめよう.みんな';
+    const orgData: OrgSchema = {
+      name: ORG_NAME,
+      memberAccessType: 'INVITE_ONLY',
+      awalaEndpoint: nonAsciiAwalaEndpoint,
+    };
+
+    await createOrg(orgData, {
+      dbConnection: connection,
+      logger: mockLogging.logger,
+    });
+
+    const orgModel = getModelForClass(OrgModelSchema, {
+      existingConnection: connection,
+    });
+    const dbResult = await orgModel.exists({
+      name: ORG_NAME,
+      memberAccessType: 'INVITE_ONLY',
+      awalaEndpoint: nonAsciiAwalaEndpoint,
+    });
+    expect(dbResult).not.toBeNull();
+  });
+
   test('Returned id should match that of the database', async () => {
     const connection = getConnection();
     const orgData: OrgSchema = {
