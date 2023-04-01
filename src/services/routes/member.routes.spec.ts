df6cc84ceceaf8a9a0ac8e287f99dcbd3cca3161
levan basharuli <l.basharuli@gmail.com>
2023-04-01 21:11:08 +0400

    fixes

--- src/services/routes/member.routes.spec.ts
+++ src/services/routes/member.routes.spec.ts
@@ -3,7 +3,7 @@ import type { InjectOptions } from 'fastify';
 import { jest } from '@jest/globals';
 
 import { configureMockEnvVars, REQUIRED_SERVER_ENV_VARS } from '../../testUtils/envVars.js';
-import { MEMBER_EMAIL, MEMBER_API_ID, MEMBER_NAME, ORG_NAME } from '../../testUtils/stubs.js';
+import { MEMBER_EMAIL, MEMBER_MONGO_ID, MEMBER_NAME, ORG_NAME } from '../../testUtils/stubs.js';
 import type { Result, SuccessfulResult } from '../../utilities/result.js';
 import { mockSpy } from '../../testUtils/jest.js';
 import { HTTP_STATUS_CODES } from '../http.js';
@@ -237,7 +237,7 @@ describe('member routes', () => {
   describe('delete', () => {
     const injectionOptions: InjectOptions = {
       method: 'DELETE',
-      url: `/orgs/${ORG_NAME}/members/${MEMBER_API_ID}`,
+      url: `/orgs/${ORG_NAME}/members/${MEMBER_MONGO_ID}`,
     };
 
     test('Valid org name and member id should be accepted', async () => {
@@ -254,7 +254,7 @@ describe('member routes', () => {
 
       const response = await serverInstance.inject(injectionOptions);
 
-      expect(mockDeleteMember).toHaveBeenCalledWith(MEMBER_API_ID, {
+      expect(mockDeleteMember).toHaveBeenCalledWith(MEMBER_MONGO_ID, {
         logger: serverInstance.log,
         dbConnection: serverInstance.mongoose,
       });
