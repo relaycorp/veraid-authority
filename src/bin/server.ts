1d2d692d08cd6adf5d64c2ba02973e7e23a25139
Gus Narea <gnarea@users.noreply.github.com>
2023-04-11 14:22:11 +0100

    update entrypoint

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,4 +1,21 @@
+import { argv } from 'node:process';
+
+import type { FastifyInstance } from 'fastify';
+
 import { makeServer } from '../api/server.js';
 import { runFastify } from '../utilities/fastify/server.js';
 
-await runFastify(await makeServer());
+type ServerMaker = () => Promise<FastifyInstance>;
+
+const SERVER_MAKERS: { [key: string]: ServerMaker } = {
+  api: makeServer,
+};
+
+const [, scriptName, serverName] = argv;
+const serverMaker = SERVER_MAKERS[serverName] as ServerMaker | undefined;
+
+if (serverMaker === undefined) {
+  throw new Error(`${scriptName}: Invalid server name (${serverName})`);
+}
+
+await runFastify(await serverMaker());
