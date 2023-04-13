4daccf620e8d05898f82eb840e5c22adb1242e9d
Gus Narea <gnarea@users.noreply.github.com>
2023-04-13 01:18:25 +0100

    tweak

--- src/utilities/fastify/plugins/fastifyMongoose.ts
+++ src/utilities/fastify/plugins/fastifyMongoose.ts
@@ -3,7 +3,12 @@ import fastifyPlugin from 'fastify-plugin';
 
 import { createMongooseConnectionFromEnv } from '../../mongo.js';
 
+const DELAY = 2000;
+
 async function fastifyMongoose(fastify: FastifyInstance): Promise<void> {
+  // eslint-disable-next-line promise/avoid-new,no-promise-executor-return
+  await new Promise((resolve) => setTimeout(resolve, DELAY));
+
   const mongooseConnection = await createMongooseConnectionFromEnv();
 
   fastify.addHook('onClose', async () => {
