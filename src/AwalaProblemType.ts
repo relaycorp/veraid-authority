aa8758d72c3c01744dcdbd5065a9d167c4679be7
levan basharuli <l.basharuli@gmail.com>
2023-04-14 23:14:26 +0400

    fixes

--- src/AwalaProblemType.ts
+++ src/AwalaProblemType.ts
@@ -1,4 +1,3 @@
 export enum AwalaProblemType {
   MALFORMED_AWALA_MESSAGE_BODY = 'https://veraid.net/problems/malformed-awala-message-body',
-  MEMBER_BUNDLE_START_DATE_PASSED = 'https://veraid.net/problems/member-bundle-start-date-passed',
 }
