bb0fd5c9c0ed87353fa195846aab57acb31f0655
Gus Narea <gnarea@users.noreply.github.com>
2023-04-17 15:51:00 +0100

    refactor

--- src/api/server.spec.ts
+++ src/api/server.spec.ts
@@ -1,25 +1,13 @@
 import { jest } from '@jest/globals';
 import pino from 'pino';
 
-import { configureMockEnvVars } from '../testUtils/envVars.js';
-
 const mockFastify = Symbol('Mock server');
 jest.unstable_mockModule('../utilities/fastify/server.js', () => ({
   makeFastify: jest.fn<() => Promise<any>>().mockResolvedValue(mockFastify),
 }));
 
-const mockMakeLogger = jest.fn().mockReturnValue({});
-jest.unstable_mockModule('../utilities/logging.js', () => ({ makeLogger: mockMakeLogger }));
-
 const { makeApiServer } = await import('./server.js');
 const { makeFastify } = await import('../utilities/fastify/server.js');
-const { REQUIRED_API_ENV_VARS } = await import('../testUtils/apiServer.js');
-
-configureMockEnvVars(REQUIRED_API_ENV_VARS);
-
-afterAll(() => {
-  jest.restoreAllMocks();
-});
 
 describe('makeApiServer', () => {
   test('No logger should be passed by default', async () => {
