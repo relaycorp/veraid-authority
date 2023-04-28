f6f49e083e7e64c64c3688eed3a30b8ef4d4bee1
levan basharuli <l.basharuli@gmail.com>
2023-04-28 16:13:41 +0400

    fixes

--- src/api/server.spec.ts
+++ src/api/server.spec.ts
@@ -1,29 +1,32 @@
 import { jest } from '@jest/globals';
 import pino from 'pino';
-import { FastifyInstance, RouteOptions } from 'fastify';
+import type { FastifyInstance } from 'fastify';
 
 import { mockSpy } from '../testUtils/jest.js';
-import { FastifyTypedInstance } from '../utilities/fastify/FastifyTypedInstance.js';
-import { PluginDone } from '../utilities/fastify/PluginDone.js';
-import { configureMockEnvVars, REQUIRED_ENV_VARS } from '../testUtils/envVars.js';
+import { configureMockEnvVars } from '../testUtils/envVars.js';
 import { AWALA_MIDDLEWARE_ENDPOINT } from '../testUtils/eventing/stubs.js';
 
-const mockFastify = Symbol('Mock server');
+
+const mockRegisterAwalaRoute = mockSpy(jest.fn());
+jest.unstable_mockModule('./routes/awala.routes.js', () => ({
+  default: mockRegisterAwalaRoute,
+}));
+
+const mockFastify: FastifyInstance = {
+  register: mockSpy(jest.fn()),
+} as any;
 jest.unstable_mockModule('../utilities/fastify/server.js', () => ({
   makeFastify: jest.fn<() => Promise<any>>().mockResolvedValue(mockFastify),
 }));
 
-const mockRegisterAwalaRoute = mockSpy(jest.fn<(_fastify: FastifyTypedInstance, _opts: RouteOptions, done: PluginDone) => void>().mockImplementation((_fastify: FastifyTypedInstance, _opts: RouteOptions, done: PluginDone) => { done() }));
-
-jest.unstable_mockModule('./routes/awala.routes.js', () => ({
-  default: mockRegisterAwalaRoute
-}));
 
 
 const { makeApiServer, makeApiServerPlugin } = await import('./server.js');
 const { makeFastify } = await import('../utilities/fastify/server.js');
 
 describe('makeApiServer', () => {
+  const mockEnvVariables = configureMockEnvVars();
+
   test('No logger should be passed by default', async () => {
     await makeApiServer();
 
@@ -44,30 +47,20 @@ describe('makeApiServer', () => {
     expect(serverInstance).toBe(mockFastify);
   });
 
-  describe("Awala endpoints", () => {
-    const mockEnvVariables = configureMockEnvVars()
-    const mockRegister = mockSpy(
-      jest.fn(),
-    );
-    const mockFastify: FastifyInstance = {
-      register: mockRegister,
-    } as any;
+  test('Existing env variable should register awala routes', async () => {
+    mockEnvVariables({ AWALA_MIDDLEWARE_ENDPOINT });
 
+    await makeApiServerPlugin(mockFastify);
 
-    test('Existing env variable should register awala routes', async() => {
-      mockEnvVariables({...REQUIRED_ENV_VARS, AWALA_MIDDLEWARE_ENDPOINT })
+    expect(mockFastify.register).toHaveBeenCalledWith(mockRegisterAwalaRoute);
+  });
 
-      await makeApiServerPlugin(mockFastify);
+  test('Missing env variable should not register awala routes', async () => {
+    mockEnvVariables({ AWALA_MIDDLEWARE_ENDPOINT: undefined });
 
-      expect(mockRegister).toHaveBeenCalledWith(mockRegisterAwalaRoute)
-    })
+    await makeApiServerPlugin(mockFastify);
 
-    test('Missing env variable should not register awala routes', async() => {
-      mockEnvVariables({...REQUIRED_ENV_VARS, AWALA_MIDDLEWARE_ENDPOINT: undefined })
+    expect(mockFastify.register).not.toHaveBeenCalledOnceWith(mockRegisterAwalaRoute);
+  });
 
-      await makeApiServerPlugin(mockFastify);
-
-      expect(mockRegister).not.toHaveBeenCalledWith(mockRegisterAwalaRoute)
-    })
-  })
 });
--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,6 @@
 import type { FastifyInstance, FastifyPluginCallback } from 'fastify';
 import type { BaseLogger } from 'pino';
+import env from 'env-var';
 
 import { makeFastify } from '../utilities/fastify/server.js';
 import type { RouteOptions } from '../utilities/fastify/RouteOptions.js';
@@ -8,17 +9,13 @@ import jwksPlugin from '../utilities/fastify/plugins/jwksAuthentication.js';
 import healthcheckRoutes from './routes/healthcheck.routes.js';
 import orgRoutes from './routes/org.routes.js';
 import awalaRoutes from './routes/awala.routes.js';
-import env from 'env-var';
 
-const ROOT_ROUTES: FastifyPluginCallback<RouteOptions>[] = [
-  healthcheckRoutes,
-  orgRoutes,
-];
+const ROOT_ROUTES: FastifyPluginCallback<RouteOptions>[] = [healthcheckRoutes, orgRoutes];
 
 export async function makeApiServerPlugin(server: FastifyInstance): Promise<void> {
   const awalaMiddlewareEndpoint = env.get('AWALA_MIDDLEWARE_ENDPOINT').asString();
-  if(awalaMiddlewareEndpoint){
-    ROOT_ROUTES.push(awalaRoutes)
+  if (awalaMiddlewareEndpoint !== undefined) {
+    ROOT_ROUTES.push(awalaRoutes);
   }
   await server.register(jwksPlugin);
   await Promise.all(ROOT_ROUTES.map((route) => server.register(route)));
