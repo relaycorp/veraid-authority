fec7b34c7690a9a52c28a1a12534837fdca11c33
levan basharuli <l.basharuli@gmail.com>
2023-04-28 16:32:22 +0400

    fixes

--- src/api/server.ts
+++ src/api/server.ts
@@ -10,15 +10,14 @@ import healthcheckRoutes from './routes/healthcheck.routes.js';
 import orgRoutes from './routes/org.routes.js';
 import awalaRoutes from './routes/awala.routes.js';
 
-const ROOT_ROUTES: FastifyPluginCallback<RouteOptions>[] = [healthcheckRoutes, orgRoutes];
-
 export async function makeApiServerPlugin(server: FastifyInstance): Promise<void> {
+  const rootRoutes: FastifyPluginCallback<RouteOptions>[] = [healthcheckRoutes, orgRoutes];
   const awalaMiddlewareEndpoint = env.get('AWALA_MIDDLEWARE_ENDPOINT').asString();
   if (awalaMiddlewareEndpoint !== undefined) {
-    ROOT_ROUTES.push(awalaRoutes);
+    rootRoutes.push(awalaRoutes);
   }
   await server.register(jwksPlugin);
-  await Promise.all(ROOT_ROUTES.map((route) => server.register(route)));
+  await Promise.all(rootRoutes.map((route) => server.register(route)));
 }
 
 export async function makeApiServer(customLogger?: BaseLogger): Promise<FastifyInstance> {
