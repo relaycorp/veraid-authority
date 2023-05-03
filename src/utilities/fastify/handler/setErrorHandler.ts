1934f08e37d6f55cd40eec9f89ce05b76b2b5049
levan basharuli <l.basharuli@gmail.com>
2023-05-03 14:03:12 +0400

    fixes

--- src/utilities/fastify/handler/setErrorHandler.ts
+++ src/utilities/fastify/handler/setErrorHandler.ts
@@ -9,12 +9,12 @@ export default function setErrorHandler(fastify: FastifyInstance): void {
       error.statusCode !== undefined &&
       error.statusCode < HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
     ) {
-      fastify.log.info(error, 'Client error');
+      fastify.log.info({ err: error }, 'Client error');
       await reply.send(error);
       return;
     }
 
-    fastify.log.error(error, internalServerError);
+    fastify.log.error({ err: error }, internalServerError);
     await reply.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).send(internalServerError);
   });
 }
