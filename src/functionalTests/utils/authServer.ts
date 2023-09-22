f16d0233d1c54f571415a3ad2c7a5210df786458
Gus Narea <gnarea@users.noreply.github.com>
2023-09-22 14:58:13 +0100

    fix mock auth

--- src/functionalTests/utils/authServer.ts
+++ src/functionalTests/utils/authServer.ts
@@ -10,14 +10,20 @@ import { post } from './http.js';
 const AUTH_SERVER_URL = await getServiceUrl('mock-authz-server');
 const AUTH_ENDPOINT_URL = `${AUTH_SERVER_URL}/default/token`;
 
-export async function authenticate(clientId: string): Promise<AuthorizationHeader> {
+export enum AuthScope {
+  SUPER_ADMIN = 'super-admin',
+  USER = 'user',
+}
+
+export async function authenticate(scope: AuthScope): Promise<AuthorizationHeader> {
   const body = {
     // eslint-disable-next-line @typescript-eslint/naming-convention,camelcase
     grant_type: 'client_credentials',
     // eslint-disable-next-line @typescript-eslint/naming-convention,camelcase
-    client_id: clientId,
+    client_id: 'client',
     // eslint-disable-next-line @typescript-eslint/naming-convention,camelcase
     client_secret: 's3cr3t',
+    scope,
   };
   const response = await post(AUTH_ENDPOINT_URL, {
     headers: new Headers([['Content-Type', 'application/x-www-form-urlencoded']]),
