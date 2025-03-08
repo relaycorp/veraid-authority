77820c10ffdf13b189e3fd93c6d315b0ba423626
Gus Narea <gnarea@users.noreply.github.com>
2025-03-08 12:34:17 +0000

    chore(deps): Upgrade @relaycorp/veraid-authority to v1.10.0

--- src/functionalTests/utils/api.ts
+++ src/functionalTests/utils/api.ts
@@ -6,5 +6,7 @@ export const API_URL = 'http://localhost:8080';
 
 export async function makeClient(scope: AuthScope): Promise<AuthorityClient> {
   const authHeader = await authenticate(scope);
-  return new AuthorityClient(API_URL, authHeader);
+  return new AuthorityClient(API_URL, authHeader, {
+    timeoutMs: 3000,
+  });
 }
