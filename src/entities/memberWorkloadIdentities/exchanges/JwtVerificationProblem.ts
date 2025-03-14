55a87b66ca9bae8e8e783367d5689fcfff7575fd
Gus Narea <gnarea@users.noreply.github.com>
2025-03-14 18:28:47 +0000

    Implement JWT verification

---
+++ src/entities/memberWorkloadIdentities/exchanges/JwtVerificationProblem.ts
@@ -0,0 +1,4 @@
+export enum JwtVerificationProblem {
+  JWKS_RETRIEVAL_ERROR = 'https://veraid.net/problems/jwt-verification-jwks-retrieval-error',
+  INVALID_JWT = 'https://veraid.net/problems/jwt-verification-invalid-jwt',
+}
